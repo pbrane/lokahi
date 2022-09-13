7ec675dcac45bad92e2d2b6df51b44261944e658
Łukasz Dywicki <luke@code-house.org>
2022-09-13 19:24:07 +0200

    Fix ignite client stuff.

--- minion-gateway/main/src/main/java/org/opennms/miniongateway/grpc/server/IgniteRemoteAsynManagerConfig.java
+++ minion-gateway/main/src/main/java/org/opennms/miniongateway/grpc/server/IgniteRemoteAsynManagerConfig.java
@@ -5,17 +5,15 @@ import org.apache.ignite.resources.IgniteInstanceResource;
 import org.opennms.core.ipc.grpc.server.manager.MinionManager;
 import org.opennms.horizon.shared.ignite.remoteasync.manager.IgniteRemoteAsyncManager;
 import org.opennms.horizon.shared.ignite.remoteasync.manager.impl.IgniteRemoteAsyncManagerFactoryImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class IgniteRemoteAsynManagerConfig {
 
-    @IgniteInstanceResource
-    private Ignite ignite;
-
     @Bean("igniteRemoteAsyncManager")
-    public IgniteRemoteAsyncManager localDetectorAdapter() {
+    public IgniteRemoteAsyncManager localDetectorAdapter(Ignite ignite) {
         IgniteRemoteAsyncManager igniteRemoteAsyncManager = new IgniteRemoteAsyncManagerFactoryImpl().create(ignite);
 
         return igniteRemoteAsyncManager;
