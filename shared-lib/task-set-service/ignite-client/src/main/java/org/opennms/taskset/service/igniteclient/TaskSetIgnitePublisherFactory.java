ecaf922153cf88f873d406d780b7267a8390d285
mfrazier <mfrazier@savoirtech.com>
2022-09-16 14:30:30 -0700

    Removing unused dependencies

--- shared-lib/task-set-service/ignite-client/src/main/java/org/opennms/taskset/service/igniteclient/TaskSetIgnitePublisherFactory.java
+++ shared-lib/task-set-service/ignite-client/src/main/java/org/opennms/taskset/service/igniteclient/TaskSetIgnitePublisherFactory.java
@@ -1,8 +1,8 @@
 package org.opennms.taskset.service.igniteclient;
 
-import org.apache.ignite.client.IgniteClient;
+import org.apache.ignite.Ignite;
 import org.opennms.taskset.service.api.TaskSetPublisher;
 
 public interface TaskSetIgnitePublisherFactory {
-    TaskSetPublisher create(IgniteClient igniteClient);
+    TaskSetPublisher create(Ignite ignite);
 }
--- shared-lib/task-set-service/ignite-client/src/main/java/org/opennms/taskset/service/igniteclient/impl/TaskSetIgnitePublisherFactoryImpl.java
+++ shared-lib/task-set-service/ignite-client/src/main/java/org/opennms/taskset/service/igniteclient/impl/TaskSetIgnitePublisherFactoryImpl.java
@@ -1,14 +1,14 @@
 package org.opennms.taskset.service.igniteclient.impl;
 
-import org.apache.ignite.client.IgniteClient;
+import org.apache.ignite.Ignite;
 import org.opennms.taskset.service.api.TaskSetPublisher;
 import org.opennms.taskset.service.igniteclient.TaskSetIgnitePublisherFactory;
 
 public class TaskSetIgnitePublisherFactoryImpl implements TaskSetIgnitePublisherFactory {
     @Override
-    public TaskSetPublisher create(IgniteClient igniteClient) {
+    public TaskSetPublisher create(Ignite ignite) {
         TaskSetIgnitePublisherImpl result = new TaskSetIgnitePublisherImpl();
-        result.setIgniteClient(igniteClient);
+        result.setIgnite(ignite);
 
         return result;
     }
