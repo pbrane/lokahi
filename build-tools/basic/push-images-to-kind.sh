54d4c9c1415909e0a5b8f7417d2703c7a95b94ee
Arthur Naseef <art@artnaseef.com>
2022-09-22 15:35:06 -0700

    basic build-tools - push the custom grafana image into kind

--- build-tools/basic/push-images-to-kind.sh
+++ build-tools/basic/push-images-to-kind.sh
@@ -2,7 +2,6 @@
 
 docker pull docker.io/bitnami/kafka:3
 docker pull docker.io/bitnami/zookeeper:3.7
-docker pull grafana/grafana
 docker pull mailhog/mailhog:latest
 docker pull nginx:1.21.6-alpine
 docker pull opennms/minion:29.0.10
@@ -13,7 +12,6 @@ docker pull busybox
 
 kind load docker-image docker.io/bitnami/kafka:3
 kind load docker-image docker.io/bitnami/zookeeper:3.7
-kind load docker-image grafana/grafana
 kind load docker-image mailhog/mailhog:latest
 kind load docker-image nginx:1.21.6-alpine
 kind load docker-image opennms/minion:29.0.10
@@ -22,6 +20,7 @@ kind load docker-image prom/prometheus
 kind load docker-image prom/pushgateway
 kind load docker-image busybox
 
+kind load docker-image "opennms/grafana-dev:local-basic"
 kind load docker-image "opennms/horizon-stream-core:local-basic"
 kind load docker-image "opennms/horizon-stream-minion:local-basic"
 kind load docker-image "opennms/horizon-stream-ui:local-basic"
