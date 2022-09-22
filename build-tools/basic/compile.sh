52f1542fb399c9c13f5a8fe27d917587fcef2634
Arthur Naseef <art@artnaseef.com>
2022-09-22 14:41:33 -0700

    Add build-tools/basic directory with scripts for building and running the K8S cluster.

--- build-tools/basic/compile.sh
+++ build-tools/basic/compile.sh
@@ -21,9 +21,12 @@
 ##
 ########################################################################################################################
 
+set -e
+
 mvn clean install -f parent-pom
 mvn clean install -f shared-lib
 mvn clean install -f minion-gateway
 mvn clean install -f minion
 mvn clean install -f rest-server
 mvn clean install -f notifications
+mvn clean install -f platform
