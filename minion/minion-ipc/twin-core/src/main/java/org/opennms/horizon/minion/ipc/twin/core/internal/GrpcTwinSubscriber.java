splatch/horizon-stream

Switching to protobuf.Any in rpc payloads.
Signed-off-by: Łukasz Dywicki <luke@code-house.org>

minion/minion-ipc/twin-core/src/main/java/org/opennms/horizon/minion/ipc/twin/core/internal/GrpcTwinSubscriber.java
@@ -28,6 +28,7 @@
 package org.opennms.horizon.minion.ipc.twin.core.internal;
 
 
+import com.google.protobuf.Any;
 import com.google.protobuf.InvalidProtocolBufferException;
 import java.io.IOException;
 import java.util.Properties;
@@ -176,7 +177,7 @@ public class GrpcTwinSubscriber extends AbstractTwinSubscriber {
 
         String rpcId = UUID.randomUUID().toString();
         RpcRequestProto rpcRequestProto = RpcRequestProto.newBuilder()
-            .setRpcContent(twinRequestProto.toByteString())
+            .setPayload(Any.pack(twinRequestProto))
             .setModuleId("twin")
             .setRpcId(rpcId)
             .build();
@@ -184,7 +185,7 @@ public class GrpcTwinSubscriber extends AbstractTwinSubscriber {
             @Override
             public void onNext(RpcResponseProto value) {
                 try {
-                    TwinResponseProto twinResponseProto = TwinResponseProto.parseFrom(value.getRpcContent());
+                    TwinResponseProto twinResponseProto = value.getPayload().unpack(TwinResponseProto.class);
                     responseHandler.onNext(twinResponseProto);
                 } catch (InvalidProtocolBufferException e) {
                     LOG.error("Error while requesting twin {}, received answer {}", twinRequestProto, value);

pbrane/lokahi

minion/minion-ipc/twin-core/src/main/java/org/opennms/horizon/minion/ipc/twin/core/internal/GrpcTwinSubscriber.java
@@ -28,6 +28,7 @@
 package org.opennms.horizon.minion.ipc.twin.core.internal;
 
 
+import com.google.protobuf.Any;
 import com.google.protobuf.InvalidProtocolBufferException;
 import com.google.protobuf.Message;
 import java.io.IOException;
@@ -117,7 +118,7 @@ public class GrpcTwinSubscriber extends AbstractTwinSubscriber implements CloudM
     private synchronized boolean sendTwinRpcRequest(TwinRequestProto twinRequestProto) {
         String rpcId = UUID.randomUUID().toString();
         RpcRequestProto rpcRequestProto = RpcRequestProto.newBuilder()
-            .setRpcContent(twinRequestProto.toByteString())
+            .setPayload(Any.pack(twinRequestProto))
             .setModuleId("twin")
             .setRpcId(rpcId)
             .build();
@@ -128,7 +129,7 @@ public class GrpcTwinSubscriber extends AbstractTwinSubscriber implements CloudM
             }
             if (rpcId.equals(response.getRpcId())) {
                 try {
-                    TwinResponseProto twinResponseProto = TwinResponseProto.parseFrom(response.getRpcContent());
+                    TwinResponseProto twinResponseProto = response.getPayload().unpack(TwinResponseProto.class);
                     handle(twinResponseProto);
                 } catch (InvalidProtocolBufferException e) {
 