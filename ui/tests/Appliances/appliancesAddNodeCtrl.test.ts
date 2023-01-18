13d8f3f334a5642842419ab17a548201dd37360f
chinh le <cle@opennms.com>
2023-01-18 16:37:54 -0500

    this test file might cause the timeout - no mount1

--- ui/tests/Appliances/appliancesAddNodeCtrl.test.ts
+++ ui/tests/Appliances/appliancesAddNodeCtrl.test.ts
@@ -6,7 +6,7 @@ import { useNodeMutations } from '@/store/Mutations/nodeMutations'
 
 describe('TESTING...', () => {
   test('true is truthy', () => {
-    console.log('**************no mount1')
+    console.log('**************no mount2')
     expect(true).toBeTruthy()
   })
 })
