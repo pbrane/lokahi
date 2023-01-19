8e1e5ef29a622945f76b06c6962c01908521c910
chinh le <cle@opennms.com>
2023-01-19 06:54:59 -0500

    run mountVillus

--- ui/tests/noMount.test.ts
+++
@@ -1,7 +0,0 @@
-describe('noMount', () => {
-  test('no mount...', () => {
-    // console.log('>>>>>>> noMount <<<<<<<<<')
-    console.log('>>>>>>> no mountVillus <<<<<<<<<')
-    expect(true).toBeTruthy()
-  })
-})
