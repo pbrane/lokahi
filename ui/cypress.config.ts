f14e0e466cbf471223acfcb09c2ff8eff227cf73
chinh le <cle@opennms.com>
2023-01-25 16:17:10 -0500

    integrate pipeline

--- ui/cypress.config.ts
+++ ui/cypress.config.ts
@@ -1,17 +1,18 @@
 import { defineConfig } from 'cypress'
-import cypressViteConfig from './cypress.vite.config'
+// import cypressViteConfig from './cypress.vite.config.ts.bk'
 
 export default defineConfig({
   component: {
     specPattern: 'cypress/component/**/*.cy.*.{js,jsx,ts,tsx}',
     devServer: {
       framework: 'vue',
-      bundler: 'vite',
-      viteConfig: cypressViteConfig
+      bundler: 'vite'
+      // viteConfig: cypressViteConfig
     }
   },
 
   e2e: {
+    baseUrl: 'https://onmshs',
     specPattern: 'cypress/e2e/**/*.cy.*.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
       // implement node event listeners here
