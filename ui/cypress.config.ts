fcd32bb84ad079b83a8209f1cb80e078b4aa8656
chinh le <cle@opennms.com>
2023-01-29 05:49:28 -0500

    HS-689 With vie env

--- ui/cypress.config.ts
+++ ui/cypress.config.ts
@@ -1,12 +1,22 @@
 import { defineConfig } from 'cypress'
+// import { env } from 'process'
 // import cypressViteConfig from './cypress.vite.config'
 import viteConfig from './vite.config'
 import { loadEnv } from 'vite'
+// import loadenv from 'loadenv'
 
 const env = loadEnv('development', process.cwd())
+// const env = loadenv.restore()
 console.log('env', env)
 
+// console.log('>>> ', Cypress.env['baseUrl'])
+
+console.log('process.env', process.env)
+
 export default defineConfig({
+  /* env: {
+    base_url: 'http://defineConfig.env'
+  }, */
   component: {
     specPattern: 'cypress/component/**/*.cy.*.{js,jsx,ts,tsx}',
     devServer: {
@@ -18,14 +28,18 @@ export default defineConfig({
   },
 
   e2e: {
+    baseUrl: env.VITE_CYPRESS_BASE_URL,
+    // baseUrl: Cypress.env['baseUrl'],
     // baseUrl: 'https://onmshs',
-    // baseUrl: env.VITE_CYPRESS_BASE_URL, // local
+    // baseUrl: 'http://localhost:8123', // local
     // baseUrl: env.VITE_CYPRESS_BASE_URL, // local
     // "base_url": "http://localhost:8123"
     // baseUrl: Cypress.env('base_url'),
     specPattern: 'cypress/e2e/**/*.cy.*.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      // console.log('process.env', process.env)
+      // console.log('config', config)
     }
   }
 })
