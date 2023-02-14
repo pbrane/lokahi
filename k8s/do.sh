fef540d9342a1809b48f6a5b6884ee4e02635e28
Jesse White <jesse@opennms.com>
2023-02-14 05:38:27 -0500

    initialize the elasticsearch template with the help of the eck-custom-resources operator

--- k8s/do.sh
+++ k8s/do.sh
@@ -2,7 +2,12 @@
 kubectl create -f https://download.elastic.co/downloads/eck/2.6.1/crds.yaml
 kubectl apply -f https://download.elastic.co/downloads/eck/2.6.1/operator.yaml
 kubectl apply -f elasticsearch-deployment.yaml
+
 kubectl apply -f querier-deployment.yaml
 kubectl apply -f querier-service.yaml
 kubectl apply -f ingester-deployment.yaml
 kubectl apply -f ingester-service.yaml
+
+helm repo add eck-custom-resources https://xco-sk.github.io/eck-custom-resources/
+helm install -f eck-custom-values.yaml eck-cr eck-custom-resources/eck-custom-resources-operator
+kubectl apply -f elasticsearch-index.yaml
