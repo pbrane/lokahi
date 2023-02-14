b28557da3431d8f4c96818d80c69f74d519976cb
Jesse White <jesse@opennms.com>
2023-02-14 04:30:02 -0500

    deploy to flow ingester in k8s

--- k8s/do.sh
+++ k8s/do.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 kubectl create -f https://download.elastic.co/downloads/eck/2.6.1/crds.yaml
 kubectl apply -f https://download.elastic.co/downloads/eck/2.6.1/operator.yaml
-kubectl apply -f elasticsearch-deployment.yaml 
-kubectl apply -f querier-deployment.yaml 
+kubectl apply -f elasticsearch-deployment.yaml
+kubectl apply -f querier-deployment.yaml
 kubectl apply -f querier-service.yaml
+kubectl apply -f ingester-deployment.yaml
+kubectl apply -f ingester-service.yaml
--- metrics-processor/timeseries/src/main/java/org/opennms/horizon/flows/FlowIngesterClient.java
+++ metrics-processor/timeseries/src/main/java/org/opennms/horizon/flows/FlowIngesterClient.java
@@ -2,12 +2,14 @@ package org.opennms.horizon.flows;
 
 import io.grpc.ManagedChannel;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.opennms.dataplatform.flows.document.FlowDocument;
 import org.opennms.dataplatform.flows.ingester.v1.IngesterGrpc;
 import org.opennms.dataplatform.flows.ingester.v1.StoreFlowDocumentRequest;
 import org.springframework.context.event.EventListener;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 
+@Slf4j
 @RequiredArgsConstructor
 public class FlowIngesterClient {
     private final ManagedChannel channel;
@@ -25,17 +27,17 @@ public class FlowIngesterClient {
 
     @EventListener(ApplicationReadyEvent.class)
     public void triggerFlowPushAfterStartup() {
-        System.out.println("MOO: Triggering flow push");
+        log.info("Triggering flow push...");
         pushFlowToIngester();
     }
 
     public void pushFlowToIngester() {
-        System.out.println("MOO: Starting!");
+        log.info("Pushing flow to ingester.");
         FlowDocument flowDocument = FlowDocument.newBuilder()
             .setApplication("sonos")
             .build();
         flowIngesterStub.storeFlowDocument(StoreFlowDocumentRequest.newBuilder()
             .setDocument(flowDocument).build());
-        System.out.println("MOO: DONE!");
+        log.info("Done pushing flow to ingester.");
     }
 }
