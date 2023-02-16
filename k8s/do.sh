73dfd55a2617edbc6e40120abfede1a14ef7f89a
Jesse White <jesse@opennms.com>
2023-02-16 04:13:41 -0500

    install kibana tooo

--- k8s/do.sh
+++ k8s/do.sh
@@ -2,6 +2,7 @@
 kubectl create -f https://download.elastic.co/downloads/eck/2.6.1/crds.yaml
 kubectl apply -f https://download.elastic.co/downloads/eck/2.6.1/operator.yaml
 kubectl apply -f elasticsearch-deployment.yaml
+kubectl apply -f kibana-deployment.yaml
 
 kubectl apply -f querier-deployment.yaml
 kubectl apply -f querier-service.yaml
