895b1747707b7bbf491b75d6fca932b6edd5d796
Christian Pape <Christian.Pape@informatik.hs-fulda.de>
2023-02-16 13:02:26 +0000

    Getting it to work

--- k8s/do.sh
+++ k8s/do.sh
@@ -12,3 +12,7 @@ kubectl apply -f ingester-service.yaml
 helm repo add eck-custom-resources https://xco-sk.github.io/eck-custom-resources/
 helm install -f eck-custom-values.yaml eck-cr eck-custom-resources/eck-custom-resources-operator
 kubectl apply -f elasticsearch-index.yaml
+
+kubectl apply -f opennms-sc.yml
+kubectl apply -f opennms-pv.yml
+kubectl -n opennms-hs patch pvc elasticsearch-data-elasticsearch-es-default-0 -p '{"spec":{"storageClassName":"local-storage"}}'
