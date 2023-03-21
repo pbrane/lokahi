ae4fa1f71d9c1954db1eed164de6967195d303ef
Alexander Chadfield <achadfield@nanthealth.com>
2023-03-21 09:35:15 -0400

    pom and test update

--- external-it/external-horizon-stream-it/src/main/java/org/opennms/horizon/it/DiscoveryTest.java
+++ external-it/external-horizon-stream-it/src/main/java/org/opennms/horizon/it/DiscoveryTest.java
@@ -1,3 +1,30 @@
+/*******************************************************************************
+ * This file is part of OpenNMS(R).
+ *
+ * Copyright (C) 2023 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2023 The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with OpenNMS(R).  If not, see:
+ *      http://www.gnu.org/licenses/
+ *
+ * For more information contact:
+ *     OpenNMS(R) Licensing <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ *******************************************************************************/
 package org.opennms.horizon.it;
 
 import io.cucumber.java.en.Then;
@@ -11,11 +38,19 @@ import io.restassured.specification.RequestSpecification;
 import org.apache.http.entity.ContentType;
 import org.apache.http.protocol.HTTP;
 import org.awaitility.Awaitility;
+import org.junit.Test;
 import org.opennms.horizon.it.gqlmodels.CreateNodeData;
 import org.opennms.horizon.it.gqlmodels.GQLQuery;
 import org.opennms.horizon.it.gqlmodels.querywrappers.CreateNodeResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
+import org.testcontainers.Testcontainers;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.Network;
+import org.testcontainers.images.builder.ImageFromDockerfile;
 
 import javax.ws.rs.core.HttpHeaders;
 import java.net.MalformedURLException;
@@ -161,5 +196,49 @@ public class DiscoveryTest {
 //========================================
 // Test Step Definitions
 //----------------------------------------
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    public static final GenericContainer<?> TARGET_CONTAINER = new GenericContainer<>(
+        new ImageFromDockerfile().withDockerfileFromBuilder(builder -> builder.from("linuxserver/openssh-server")
+            .build()))
+        .withExposedPorts(2222)
+        .withNetwork(Network.SHARED)
+        .withNetworkAliases("minion");
+
+    public static final GenericContainer<?> MINION_CONTAINER = new GenericContainer<>(
+        new ImageFromDockerfile().withDockerfileFromBuilder(builder -> builder.from("opennms/horizon-stream-minion")
+            .build()))
+        .withExposedPorts(8990)
+        .withEnv("MINION_ID", "minion-automation")
+        .withEnv("MINION_LOCATION", "minion-automation")
+        .withEnv("IGNITE_SERVER_ADDRESSES", "minion")
+        .withEnv("MINION_GATEWAY_HOST", "host.testcontainers.internal")
+        .withEnv("MINION_GATEWAY_PORT", "8990")
+        .withEnv("MINION_GATEWAY_TLS", "false")
+        .withNetwork(Network.SHARED)
+        .withNetworkAliases("minion");
+
+    @Test
+    public void testContainerRun() {
+        Testcontainers.exposeHostPorts(8990);
+
+        TARGET_CONTAINER.start();
+        MINION_CONTAINER.start();
+
+        String[] args = new String[0];
+
+        try {
+            SpringApplication.run(DiscoveryTest.class, args);
+        } catch (Exception exc) {
+            exc.printStackTrace();
+        }
+
+        System.out.println();
+
+    }
+
+
 
 }
