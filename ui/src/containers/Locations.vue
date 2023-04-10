bcf08e4664cdaa701b0e16b8280b1f179e87d4c0
chinh le <cle@opennms.com>
2023-04-10 06:56:30 -0400

    HS-1402 Add main page minimal setup

--- ui/src/containers/Locations.vue
+++ ui/src/containers/Locations.vue
@@ -2,20 +2,48 @@
   <div class="wrapper">
     <div class="header">
       <PageHeadline
-        text="Headline"
+        text="Locations"
         class="page-headline"
       />
-      <div>buttons, links,...</div>
+      <div><AppliancesNotificationsCtrl /></div>
     </div>
     <div class="content">
-      <div class="content-left">content left</div>
-      <div class="content-right">content right</div>
+      <div class="content-left">
+        <FeatherButton
+          primary
+          @click="addLocation"
+          data-test="add-location-btn"
+        >
+          <FeatherIcon :icon="icons.Add" />
+          Location
+        </FeatherButton>
+        <hr />
+        <!-- search input -->
+        <!-- existing locations list -->
+      </div>
+      <div class="content-right">
+        <!-- minions list of a location -->
+        <!--  header -->
+        <!--  card list -->
+        <!-- OR -->
+        <!-- location add form -->
+        <!--  header -->
+        <!--  inputs, actions -->
+        <!--  footer: cancel, save -->
+      </div>
     </div>
-    <div class="footer">footer - if needed</div>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import Add from '@featherds/icon/action/Add'
+
+const addLocation = () => ({})
+
+const icons = markRaw({
+  Add
+})
+</script>
 
 <style lang="scss" scoped>
 @use '@/styles/layout/headlineTwoColumns';
