ad91a8893b7a3c560c826fb7e2f0bb63abd173f9
Gerald Humphries <geraldhumphries@gmail.com>
2023-05-15 15:00:48 -0400

    Add cert generation to run-cluster script
    Fix HS-1220
    
    Co-authored-by: Łukasz Dywicki <luke@code-house.org>

--- build-tools/basic/run-cluster.sh
+++ build-tools/basic/run-cluster.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+./install-local/load-or-generate-secret.sh "default" "root-ca-certificate"  "opennms-ca" "target/tmp/server-ca.key" "target/tmp/server-ca.crt"
+./install-local/generate-and-sign-certificate.sh "default" "opennms-minion-gateway-certificate" "minion.onmshs.local" "target/tmp/server-ca.key" "target/tmp/server-ca.crt"
+./install-local/generate-and-sign-certificate.sh "default" "opennms-ui-certificate" "onmshs.local" "target/tmp/server-ca.key" "target/tmp/server-ca.crt"
+./install-local/load-or-generate-secret.sh "default" "client-root-ca-certificate" "client-ca" "target/tmp/client-ca.key" "target/tmp/client-ca.crt"
 helm upgrade -i opennms ./charts/opennms -f tilt-helm-values.yaml \
   --set Grafana.Image=opennms/horizon-stream-grafana:local-basic \
   --set Keycloak.Image=opennms/horizon-stream-keycloak:local-basic \
