9074c6e49b3e28dc4fcb4465e8d6cc6026a3f1e5
DJ Gregor <dj@gregor.com>
2023-07-16 18:07:39 -0400

    BTO-615: minion: Update default env vars in Dockerfile

--- minion/docker-assembly/src/main/docker/entrypoint/entrypoint.sh
+++ minion/docker-assembly/src/main/docker/entrypoint/entrypoint.sh
@@ -8,7 +8,8 @@ trap 's=$?; echo >&2 "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
 
 if ! [[ -v GRPC_CLIENT_KEYSTORE ]]; then
 	echo "Required keystore file environment variable 'GRPC_CLIENT_KEYSTORE' not set." >&2
-	echo "Example docker argument to pass keystore file: -e GRPC_CLIENT_KEYSTORE=/opt/karaf/minion.p12" >&2
+	echo "This should always be set by the Dockerfile, so this error is unexpected." >&2
+	echo "Workaround: example docker argument to pass keystore file: -e GRPC_CLIENT_KEYSTORE=/opt/karaf/minion.p12" >&2
 	exit 1
 fi
 
