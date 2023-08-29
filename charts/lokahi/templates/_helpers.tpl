pbrane/lokahi

commit 15c2f4a6fcc1742a8823ac41a1f2c6d951f2aceb

Helm chart: allow setting the image version in one place
Also had to tweak how the existing "deployment.env" define is
included since it how has to call the new "lokahi.image" define.

You can only pass one set of values with include, but I found this
great hack where you can build a custom dictionary to both give
access to global values, but also local values:
https://stackoverflow.com/a/52648558

We use this to pass a reference to the top of the Values tree,
and also to a service-specific part of the tree (for example:
Values.OpenNMS.Alert).

charts/lokahi/templates/_helpers.tpl
@@ -1,11 +1,41 @@
+{{- /*
+lokahi.image: return the docker image name including tag for the service
+argument: a dictionary with Values and "thisService" rooted at the values
+for the service.
+example:
+	image: {{ include "lokahi.image" (dict "Values" .Values "thisService" .Values.OpenNMS.Alert) | quote }}
+
+Precedence for image (first wins):
+1. thisService.Image
+2. OpenNMS.global.image.repository + "/" + image short name + "/" + tag
+
+image short name:
+1. thisService.ImageShortName
+2. thisService.ServiceName
+
+tag:
+1. OpenNMS.global.image.tag
+2. "latest"
+
+TODO: The default tag should eventually be .Chart.AppVersion, but we
+aren't versioning the chart yet.
+*/}}
+{{- define "lokahi.image" }}
+{{- if .thisService.Image -}}
+{{- .thisService.Image -}}
+{{- else -}}
+{{- $imageShortName := .thisService.ImageShortName | default .thisService.ServiceName -}}
+{{- $tag := .Values.OpenNMS.global.image.tag | default "latest" -}}
+{{- printf "%s/%s:%s" .Values.OpenNMS.global.image.repository $imageShortName $tag -}}
+{{- end -}}
+{{- end -}}
+
 {{- define "deployment.env" -}}
   {{- /* OpenTelemetry environment variables */ -}}
 - name: OTEL_SERVICE_NAME
-  value: {{ .ServiceName | quote }}
-  {{- if (regexMatch ".*:" .Image) }}
+  value: {{ .thisService.ServiceName | quote }}
 - name: OTEL_RESOURCE_ATTRIBUTES
-  value: {{ printf "service.version=%s" (regexReplaceAllLiteral ".*:" .Image "") | quote }}
-  {{- end }}
+  value: {{ printf "service.version=%s" (regexReplaceAllLiteral ".*:" (include "lokahi.image" .) "") | quote }}
   {{- /* Other environment variables */ -}}
   {{- if .env }}
     {{- range $key, $val := .env }}

charts/lokahi/templates/grafana/grafana-deployment.yaml
@@ -44,7 +44,7 @@ spec:
             defaultMode: 420
       containers:
         - name: {{ .Values.Grafana.ServiceName }}
-          image: {{ .Values.Grafana.Image }}
+          image: {{ include "lokahi.image" (dict "Values" .Values "thisService" .Values.Grafana) | quote }}
           imagePullPolicy: {{ .Values.Grafana.ImagePullPolicy }}
           ports:
             - containerPort: {{ .Values.Grafana.Port }}

charts/lokahi/templates/keycloak/keycloak-deployment.yaml
@@ -42,7 +42,7 @@ spec:
       {{- end }}
       containers:
         - name: keycloak
-          image: "{{ .Values.Keycloak.Image }}"
+          image: {{ include "lokahi.image" (dict "Values" .Values "thisService" .Values.Keycloak) | quote }}
           imagePullPolicy: "{{ .Values.Keycloak.ImagePullPolicy }}"
           resources:
             limits:
@@ -134,7 +134,7 @@ spec:
               value: {{ $val | quote }}
               {{- end }}
             {{- end }}
-            {{- include "deployment.env" .Values.Keycloak | nindent 12 }}
+            {{- include "deployment.env" (dict "Values" .Values "thisService" .Values.Keycloak) | nindent 12 }}
           ports:
             - name: http
               containerPort: {{ .Values.Keycloak.Port }}

charts/lokahi/templates/opennms/alert/alert-deployment.yaml
@@ -48,7 +48,7 @@ spec:
             name: spring-boot-app-config
       containers:
         - name: {{ .Values.OpenNMS.Alert.ServiceName }}
-          image: {{ .Values.OpenNMS.Alert.Image }}
+          image: {{ include "lokahi.image" (dict "Values" .Values "thisService" .Values.OpenNMS.Alert) | quote }}
           imagePullPolicy: {{ .Values.OpenNMS.Alert.ImagePullPolicy }}
           env:
             - name: JAVA_TOOL_OPTIONS
@@ -67,7 +67,7 @@ spec:
                   key: alertPwd
             - name: SPRING_KAFKA_BOOTSTRAP_SERVERS
               value: "{{ .Values.OpenNMS.global.kafkaClient.bootstrapServers }}"
-            {{- include "deployment.env" .Values.OpenNMS.Alert | nindent 12 }}
+            {{- include "deployment.env" (dict "Values" .Values "thisService" .Values.OpenNMS.Alert) | nindent 12 }}
           envFrom:
           - configMapRef:
               name: spring-boot-env

charts/lokahi/templates/opennms/api/api-deployment.yaml
@@ -52,7 +52,7 @@ spec:
             name: spring-boot-app-config
       containers:
         - name: {{ .Values.OpenNMS.API.ServiceName }}
-          image: {{ .Values.OpenNMS.API.Image }}
+          image: {{ include "lokahi.image" (dict "Values" .Values "thisService" .Values.OpenNMS.API) | quote }}
           imagePullPolicy: {{ .Values.OpenNMS.API.ImagePullPolicy }}
           env:
             - name: JAVA_TOOL_OPTIONS
@@ -83,7 +83,7 @@ spec:
             - name: GRPC_URL_FLOWS_TLS_ENABLED
               value: "{{ .Values.OpenNMS.API.FlowsTlsEnabled }}"
 
-            {{- include "deployment.env" .Values.OpenNMS.API | nindent 12 }}
+            {{- include "deployment.env" (dict "Values" .Values "thisService" .Values.OpenNMS.API) | nindent 12 }}
           envFrom:
           - configMapRef:
               name: spring-boot-env

charts/lokahi/templates/opennms/datachoices/datachoices-deployment.yaml
@@ -45,7 +45,7 @@ spec:
             name: spring-boot-app-config
       containers:
         - name: {{ .Values.OpenNMS.DataChoices.ServiceName }}
-          image: {{ .Values.OpenNMS.DataChoices.Image }}
+          image: {{ include "lokahi.image" (dict "Values" .Values "thisService" .Values.OpenNMS.DataChoices) | quote }}
           imagePullPolicy: {{ .Values.OpenNMS.DataChoices.ImagePullPolicy }}
           env:
             - name: JAVA_TOOL_OPTIONS
@@ -68,7 +68,7 @@ spec:
               value: "http://{{ .Values.Keycloak.ServiceName }}:8080/auth/"
             - name: KEYCLOAK_REALM
               value: "{{ .Values.Keycloak.RealmName }}"
-            {{- include "deployment.env" .Values.OpenNMS.DataChoices | nindent 12 }}
+            {{- include "deployment.env" (dict "Values" .Values "thisService" .Values.OpenNMS.DataChoices) | nindent 12 }}
           envFrom:
           - configMapRef:
               name: spring-boot-env

charts/lokahi/templates/opennms/events/events-deployment.yaml
@@ -48,7 +48,7 @@ spec:
             name: spring-boot-app-config
       containers:
         - name: {{ .Values.OpenNMS.Events.ServiceName }}
-          image: {{ .Values.OpenNMS.Events.Image }}
+          image: {{ include "lokahi.image" (dict "Values" .Values "thisService" .Values.OpenNMS.Events) | quote }}
           imagePullPolicy: {{ .Values.OpenNMS.Events.ImagePullPolicy }}
           env:
             - name: JAVA_TOOL_OPTIONS
@@ -69,7 +69,7 @@ spec:
               value: "{{ .Values.OpenNMS.global.kafkaClient.bootstrapServers }}"
             - name: GRPC_URL_INVENTORY
               value: "{{ .Values.OpenNMS.Inventory.ServiceName }}:6565"
-            {{- include "deployment.env" .Values.OpenNMS.Events | nindent 12 }}
+            {{- include "deployment.env" (dict "Values" .Values "thisService" .Values.OpenNMS.Events) | nindent 12 }}
           envFrom:
           - configMapRef:
               name: spring-boot-env

charts/lokahi/templates/opennms/inventory/inventory-deployment.yaml
@@ -48,7 +48,7 @@ spec:
             name: spring-boot-app-config
       containers:
         - name: {{ .Values.OpenNMS.Inventory.ServiceName }}
-          image: {{ .Values.OpenNMS.Inventory.Image }}
+          image: {{ include "lokahi.image" (dict "Values" .Values "thisService" .Values.OpenNMS.Inventory) | quote }}
           imagePullPolicy: {{ .Values.OpenNMS.Inventory.ImagePullPolicy }}
           env:
             - name: JAVA_TOOL_OPTIONS
@@ -76,7 +76,7 @@ spec:
                 secretKeyRef:
                   key: encryptionKey
                   name: {{ .Values.OpenNMS.Inventory.ServiceName }}-encryption-key
-            {{- include "deployment.env" .Values.OpenNMS.Inventory | nindent 12 }}
+            {{- include "deployment.env" (dict "Values" .Values "thisService" .Values.OpenNMS.Inventory) | nindent 12 }}
           envFrom:
           - configMapRef:
               name: spring-boot-env

commit f6327ccb37e95bcd2b5e303b8cf5641dbccf108a

Fix 'env' values on individual services
Broke this functionality in 15c2f4a6fcc1742a8823ac41a1f2c6d951f2aceb.

charts/lokahi/templates/_helpers.tpl
@@ -53,8 +53,8 @@ by adding them as key/value pairs under <ServiceName>.env.
 - name: OTEL_RESOURCE_ATTRIBUTES
   value: {{ printf "service.version=%s" (regexReplaceAllLiteral ".*:" (include "lokahi.image" .) "") | quote }}
   {{- /* Other environment variables */ -}}
-  {{- if .env }}
-    {{- range $key, $val := .env }}
+  {{- if .thisService.env }}
+    {{- range $key, $val := .thisService.env }}
 - name: {{ $key }}
   value: {{ $val | quote }}
     {{- end }}