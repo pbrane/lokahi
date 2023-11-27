ea89a557aaa6e99db1f5108192fee7fc2090e980
Pierre Bouffard <39974723+bouff@users.noreply.github.com>
2023-11-27 15:45:38 -0500

    LOK-2161: Remove Operator from Lokahi (#1540)

--- operator/scripts/create-instance.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#use this script to install a basic version of OpenNMS Horizon Stream locally
-
-echo
-echo ____________Installing Local Instance______________
-echo
-kubectl apply -f scripts/local-onms-instance.yaml
-if [ $? -ne 0 ]; then exit; fi
-
