607a4f237e67401ea10ec2ca01a8e746b54f6076
Scott Theleman <synqotik@users.noreply.github.com>
2024-03-05 11:50:26 -0500

    LOK-2333: Suppress calling flows API until back end is implemented (#1685)
    This will prevent snackbar error message from popping up on Node Status page

--- ui/src/store/Views/nodeStatusStore.ts
+++ ui/src/store/Views/nodeStatusStore.ts
@@ -1,8 +1,9 @@
 import { defineStore } from 'pinia'
+import { FLOWS_ENABLED } from '@/constants'
 import { useNodeStatusQueries } from '@/store/Queries/nodeStatusQueries'
-import { useNodeMutations } from '../Mutations/nodeMutations'
 import { AZURE_SCAN, DeepPartial } from '@/types'
 import { Exporter, NodeUpdateInput, RequestCriteriaInput } from '@/types/graphql'
+import { useNodeMutations } from '../Mutations/nodeMutations'
 
 export const useNodeStatusStore = defineStore('nodeStatusStore', () => {
   const nodeStatusQueries = useNodeStatusQueries()
@@ -10,6 +11,7 @@ export const useNodeStatusStore = defineStore('nodeStatusStore', () => {
   const fetchedData = computed(() => nodeStatusQueries.fetchedData)
   const exporters = ref<DeepPartial<Exporter>[]>([])
   const nodeId = ref()
+
   const setNodeId = (id: number) => {
     nodeStatusQueries.setNodeId(id)
     nodeId.value = id
@@ -30,8 +32,13 @@ export const useNodeStatusStore = defineStore('nodeStatusStore', () => {
         endTime
       }
     }
-    const data = await nodeStatusQueries.fetchExporters(payload)
-    exporters.value = data.value?.findExporters || []
+
+    if (FLOWS_ENABLED) {
+      const data = await nodeStatusQueries.fetchExporters(payload)
+      exporters.value = data.value?.findExporters || []
+    } else {
+      exporters.value = []
+    }
   }
 
   // add the exporter object to the matching node snmp interface (match on ifIndex)
