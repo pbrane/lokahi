cbc0782f14c13fca871b8dbfd023d38cf813f5a1
aurang-zeb313 <159940154+aurang-zeb313@users.noreply.github.com>
2024-04-15 16:41:17 +0500

    LOK-2441 : Fill up name field in Event defs based on uei (#1787)
    * LOK-2441: BE: Fill up name field in Event defs based on uei
    
    * LOK-2441: Run spotless

--- rest-server/src/main/java/org/opennms/horizon/server/service/GraphQLAlertService.java
+++ rest-server/src/main/java/org/opennms/horizon/server/service/GraphQLAlertService.java
@@ -30,6 +30,7 @@ import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
@@ -167,7 +168,21 @@ public class GraphQLAlertService {
     @GraphQLQuery(name = "alertEventDefsByVendor")
     public Mono<EventDefinitionsByVendor> listEventDefinitionsByVendor(
             @GraphQLArgument EventDefsByVendorRequest request, @GraphQLEnvironment ResolutionEnvironment env) {
-        return Mono.just(alertsClient.listAlertEventDefinitionsByVendor(request, headerUtil.getAuthHeader(env)));
+
+        EventDefinitionsByVendor eventsDefinitionsByVendor =
+                alertsClient.listAlertEventDefinitionsByVendor(request, headerUtil.getAuthHeader(env));
+        eventsDefinitionsByVendor.getAlertEventDefinitionList().stream().forEach(aed -> {
+            if (aed.getUei() != null && aed.getUei().contains("/")) {
+                String lastElement = Arrays.stream(aed.getUei().split("/"))
+                        .reduce((first, second) -> second)
+                        .orElse(null);
+                aed.setName(lastElement);
+            }
+            aed.setClearKey(aed.getClearKey().isEmpty() ? null : aed.getClearKey());
+            aed.setReductionKey(aed.getReductionKey().isEmpty() ? null : aed.getReductionKey());
+        });
+
+        return Mono.just(eventsDefinitionsByVendor);
     }
 
     @GraphQLQuery(name = "listVendors")
