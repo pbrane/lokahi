4d6a049d7de3910b5dafffa1374a60f44444fb40
Mohammad-Junid <161852172+Mohammad-Junid@users.noreply.github.com>
2024-04-23 20:33:34 +0500

    Implement NTP Service Montor (#1770)
    * add NTP monitor service through command shell to minion
    
    * add implementation of NTP service
    
    * run ntp monitor through karaf command
    
    * refactor code
    
    * add unit test case
    
    * minor change in monitor
    
    * ad all tst cases and refactor ntp monitor code
    
    * apply spotless check
    
    * move  the thread pool model code from TaskExecutorLocalMonitorServiceImpl  to  TaskExecutorLocalServiceFactoryImpl
    
    * fix issue in test case
    
    * Do review chaneges
    
    * Apply spotless
    
    * change proto package name and apply spotless
    
    * removed unused code
    
    * remove wrong logs message

---
+++ minion/plugins/ntp-plugin/src/test/java/org/opennms/lokahi/minion/ntp/NtpMonitorTest.java
@@ -0,0 +1,119 @@
+/*
+ * Licensed to The OpenNMS Group, Inc (TOG) under one or more
+ * contributor license agreements.  See the LICENSE.md file
+ * distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * TOG licenses this file to You under the GNU Affero General
+ * Public License Version 3 (the "License") or (at your option)
+ * any later version.  You may not use this file except in
+ * compliance with the License.  You may obtain a copy of the
+ * License at:
+ *
+ *      https://www.gnu.org/licenses/agpl-3.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied.  See the License for the specific
+ * language governing permissions and limitations under the
+ * License.
+ */
+package org.opennms.lokahi.minion.ntp;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import com.google.protobuf.Any;
+import java.util.Arrays;
+import java.util.concurrent.CompletableFuture;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.opennms.horizon.minion.plugin.api.MonitoredService;
+import org.opennms.horizon.minion.plugin.api.ServiceMonitorResponse;
+import org.opennms.monitors.ntp.contract.NTPMonitorRequest;
+
+public class NtpMonitorTest {
+
+    private static final String DEFAULT_TEST_INVALID_IP_ADDRESS_VALUE = "time14.google.com";
+
+    private static final String DEFAULT_TEST_IP_ADDRESS_VALUE = "time.google.com";
+
+    private static final int DEFAULT_TEST_PORT_VALUE = 123;
+
+    private static final int DEFAULT_TEST_RETRIES_VALUE = 3;
+
+    private static final int DEFAULT_TEST_TIMEOUT_VALUE = 5000;
+
+    private static final int NON_DEFAULT_TEST_PORT_VALUE = 124;
+
+    @Mock
+    MonitoredService monitoredService;
+
+    NTPMonitorRequest ntpMonitorRequest;
+
+    Any testConfig;
+    NtpMonitor ntpMonitor;
+
+    @Before
+    public void setUp() throws Exception {
+        ntpMonitor = new NtpMonitor();
+
+        MockitoAnnotations.openMocks(this);
+        ntpMonitorRequest = NTPMonitorRequest.newBuilder()
+                .setInetAddress(DEFAULT_TEST_IP_ADDRESS_VALUE)
+                .build();
+
+        testConfig = Any.pack(ntpMonitorRequest);
+    }
+
+    @Test
+    public void pollSuccess() throws Exception {
+        testConfig = Any.pack(getNtpMonitorRequest(
+                DEFAULT_TEST_IP_ADDRESS_VALUE,
+                DEFAULT_TEST_PORT_VALUE,
+                DEFAULT_TEST_RETRIES_VALUE,
+                DEFAULT_TEST_TIMEOUT_VALUE));
+        CompletableFuture<ServiceMonitorResponse> response = ntpMonitor.poll(monitoredService, testConfig);
+        ServiceMonitorResponse serviceMonitorResponse = response.get();
+        assertEquals(ServiceMonitorResponse.Status.Up, serviceMonitorResponse.getStatus());
+        assertTrue(serviceMonitorResponse.getResponseTime() > 0.0);
+    }
+
+    @Test
+    public void pollUnknownHostException() throws Exception {
+        testConfig = Any.pack(getNtpMonitorRequest(
+                DEFAULT_TEST_INVALID_IP_ADDRESS_VALUE,
+                DEFAULT_TEST_PORT_VALUE,
+                DEFAULT_TEST_RETRIES_VALUE,
+                DEFAULT_TEST_TIMEOUT_VALUE));
+        CompletableFuture<ServiceMonitorResponse> response = ntpMonitor.poll(monitoredService, testConfig);
+        ServiceMonitorResponse serviceMonitorResponse = response.get();
+        assertEquals(ServiceMonitorResponse.Status.Down, serviceMonitorResponse.getStatus());
+        assertTrue(serviceMonitorResponse.getResponseTime() <= 0.0);
+    }
+
+    @Test
+    public void pollTimeOutException() throws Exception {
+        testConfig = Any.pack(getNtpMonitorRequest(
+                DEFAULT_TEST_IP_ADDRESS_VALUE,
+                NON_DEFAULT_TEST_PORT_VALUE,
+                DEFAULT_TEST_RETRIES_VALUE,
+                DEFAULT_TEST_TIMEOUT_VALUE));
+        CompletableFuture<ServiceMonitorResponse> response = ntpMonitor.poll(monitoredService, testConfig);
+        ServiceMonitorResponse serviceMonitorResponse = response.get();
+        assertEquals(ServiceMonitorResponse.Status.Unknown, serviceMonitorResponse.getStatus());
+        assertTrue(serviceMonitorResponse.getResponseTime() <= 0.0);
+    }
+
+    public NTPMonitorRequest getNtpMonitorRequest(String ipAddress, int port, int retries, int timeout) {
+        return NTPMonitorRequest.newBuilder()
+                .setInetAddress(ipAddress)
+                .addAllPort(Arrays.asList(port))
+                .setRetries(retries)
+                .setTimeout(timeout)
+                .build();
+    }
+}
